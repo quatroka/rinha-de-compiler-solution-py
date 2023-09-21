2794263e0f27d6545fd674f80ec31473fedcdcdd
quatroka <monteiro.leonardosantos@gmail.com>
2023-09-21 03:45:16 -0300

    fix test

--- test/test_fib.py
+++ test/test_fib.py
@@ -2,6 +2,6 @@ from pytest import CaptureFixture
 from src.interpreter import process_file
 
 
-def test_fib(capfd: CaptureFixture):
-    process_file("./files/fib.json")
+def test_fib10(capfd: CaptureFixture):
+    process_file("./files/fib10.json")
     assert "55\n" == capfd.readouterr().out
