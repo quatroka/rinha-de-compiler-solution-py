6969791a82000ac28f8f094fbcb8507d83c845d3
quatroka <monteiro.leonardosantos@gmail.com>
2023-09-22 13:04:49 -0300

    test case of conversion of bool in str

--- test/generated_by_me/test_print.py
+++ test/generated_by_me/test_print.py
@@ -13,3 +13,7 @@ def test_print2(capfd: CaptureFixture):
 def test_print3(capfd: CaptureFixture):
     process_file("./files/generated_by_me/print3.rinha.json")
     assert "prefix: oi\nprefix: a\nprefix: 2\nprefix: true\nprefix: 1\n" == capfd.readouterr().out
+
+def test_print4(capfd: CaptureFixture):
+    process_file("./files/generated_by_me/print4.rinha.json")
+    assert "true\nfalse\nfalse\ntrue\n---\ntrue\nfalse\nfalse\ntrue\n" == capfd.readouterr().out
